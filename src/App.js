0a4be6a0f3d7a0c639250b2d02ed7db135de4d8d
fajar13k <fajar.fj13@gmail.com>
2020-05-31 06:24:00 +0700

    Add datepicker

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
+import DatePicker from "react-datepicker";
+
 import Logo from './assets/logo-meetup.svg';
 import Avatar from './assets/avatar.jpg';
+import "react-datepicker/dist/react-datepicker.css";
 
 class App extends Component {
+
+  state = {
+    startDate: new Date()
+  };
+
+  handleChange = date => {
+    this.setState({
+      startDate: date
+    });
+  };
+
   render() {
     return (
       <div className="font-sans bg-gray-200">
@@ -191,11 +205,16 @@ class App extends Component {
               <button className="bg-white px-4 py-1 text-sm border border-gray-500">Today</button>
             </div>
 
-            <div>
-              Calendar here
+            <div className="w-full mb-8">
+              <DatePicker
+                todayButton="Today"
+                selected={this.state.startDate}
+                onChange={this.handleChange}
+                inline
+              />
             </div>
 
-            <div className="text-center flex justify-center mb-8">
+            <div className="text-center flex justify-center">
               <a href="/" className="flex">
                 <svg className="mr-2" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path className="heroicon-ui" d="M19 6.41L8.7 16.71a1 1 0 1 1-1.4-1.42L17.58 5H14a1 1 0 0 1 0-2h6a1 1 0 0 1 1 1v6a1 1 0 0 1-2 0V6.41zM17 14a1 1 0 0 1 2 0v5a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V7c0-1.1.9-2 2-2h5a1 1 0 0 1 0 2H5v12h12v-5z"></path></svg>
                 <span>Export to...</span>

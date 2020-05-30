028396919b7f93ac3623ec853aeba6fc87c91c73
fajar13k <fajar.fj13@gmail.com>
2020-05-31 05:22:45 +0700

    Base CRA

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        'meetup-blue': '#00455D',
+        'meetup-purple': '#1F24CC'
+      },
+      spacing: {
+        '72': '18rem',
+      },
+      width: {
+        '7/10': '70%',
+        '3/10': '30%',
+      }
+    },
+    fontFamily: {
+      sans: [
+        'system-ui',
+        '-apple-system',
+        'BlinkMacSystemFont',
+        '"Segoe UI"',
+        'Roboto',
+        '"Helvetica Neue"',
+        'Arial',
+        '"Noto Sans"',
+        'sans-serif',
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+        '"Noto Color Emoji"',
+      ],
+    }
+  },
+  variants: {},
+  plugins: [],
+}

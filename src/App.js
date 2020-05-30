74c6b29d603eb1cf8150924876d3437c2ce86532
fajar13k <fajar.fj13@gmail.com>
2020-05-31 05:48:03 +0700

    Add main content

--- src/App.js
+++ src/App.js
@@ -56,6 +56,155 @@ class App extends Component {
           </div>
         </div>
 
+        {/* Container for main content */}
+        <div className="container mx-auto px-40 flex flex-col flex-col-reverse lg:flex-row">
+          <div className="w-full lg:w-7/10">
+
+            <div className="pl-3 uppercase font-bold text-sm py-2 pb-4">
+              Saturday, May 25
+            </div>
+
+            <div className="events border border-gray-400 bg-white mb-8">
+              <div className="event border-b border-gray-400 flex p-4">
+                <div className="w-16 mr-4 text-gray-600"><a href="/">6:00 PM</a></div>
+                <div>
+                  <div className="uppercase font-bold text-gray-700 text-sm"><a href="/">Mississauga .NET user group</a></div>
+                  <div className="font-bold text-gray-800 text-xl"><a href="/">Visual Studio 2019 Launch Party</a></div>
+                  <div className="text-gray-600">31 .NET coders going</div>
+                </div>
+              </div>
+              <div className="event border-b border-gray-400 flex p-4">
+                <div className="w-16 mr-4 text-gray-600"><a href="/">6:00 PM</a></div>
+                <div>
+                  <div className="uppercase font-bold text-gray-700 text-sm"><a href="/">Mississauga .NET user group</a></div>
+                  <div className="font-bold text-gray-800 text-xl"><a href="/">Visual Studio 2019 Launch Party</a></div>
+                  <div className="text-gray-600">31 .NET coders going</div>
+                </div>
+              </div>
+              <div className="event flex p-4">
+                <div className="w-16 mr-4 text-gray-600"><a href="/">6:00 PM</a></div>
+                <div>
+                  <div className="uppercase font-bold text-gray-700 text-sm"><a href="/">Mississauga .NET user group</a></div>
+                  <div className="font-bold text-gray-800 text-xl"><a href="/">Visual Studio 2019 Launch Party</a></div>
+                  <div className="text-gray-600">31 .NET coders going</div>
+                </div>
+              </div>
+            </div>
+
+            <div className="pl-3 uppercase font-bold text-sm py-2 pb-4">
+              Sunday, May 26
+            </div>
+
+            <div className="events border border-gray-400 bg-white mb-8">
+              <div className="event border-b border-gray-400 flex p-4">
+                <div className="w-16 mr-4 text-gray-600"><a href="/">6:00 PM</a></div>
+                <div>
+                  <div className="uppercase font-bold text-gray-700 text-sm"><a href="/">Mississauga .NET user group</a></div>
+                  <div className="font-bold text-gray-800 text-xl"><a href="/">Visual Studio 2019 Launch Party</a></div>
+                  <div className="text-gray-600">31 .NET coders going</div>
+                </div>
+              </div>
+              <div className="event border-b border-gray-400 flex p-4">
+                <div className="w-16 mr-4 text-gray-600"><a href="/">6:00 PM</a></div>
+                <div>
+                  <div className="uppercase font-bold text-gray-700 text-sm"><a href="/">Mississauga .NET user group</a></div>
+                  <div className="font-bold text-gray-800 text-xl"><a href="/">Visual Studio 2019 Launch Party</a></div>
+                  <div className="text-gray-600">31 .NET coders going</div>
+                </div>
+              </div>
+              <div className="event flex p-4">
+                <div className="w-16 mr-4 text-gray-600"><a href="/">6:00 PM</a></div>
+                <div>
+                  <div className="uppercase font-bold text-gray-700 text-sm"><a href="/">Mississauga .NET user group</a></div>
+                  <div className="font-bold text-gray-800 text-xl"><a href="/">Visual Studio 2019 Launch Party</a></div>
+                  <div className="text-gray-600">31 .NET coders going</div>
+                </div>
+              </div>
+            </div>
+
+            <div className="border-t border-b border-gray-400 py-8 mb-8">
+              <div className="text-center font-bold text-2xl mb-2">
+                Host events in Mississauga
+              </div>
+              <div className="text-center mb-8">Can't find what you're looking for? Create a Meetup group to start hosting local events.</div>
+
+              <div className="flex flex-col sm:flex-row -mx-2">
+                <div className="w-full sm:w-1/2 mx-2 bg-white shadow mb-4">
+                  <div className="px-6 py-4">
+                    <div className="font-bold"><a href="/">Usability</a></div>
+                    <div className="text-gray-700 mb-1">593 Follow this topic</div>
+                    <div><a href="/" className="text-teal-600 hover:underline">Host events</a></div>
+                  </div>
+                </div>
+
+                <div className="w-full sm:w-1/2 mx-2 bg-white shadow mb-4">
+                  <div className="px-6 py-4">
+                    <div className="font-bold"><a href="/">Web Development</a></div>
+                    <div className="text-gray-700 mb-1">11,079 follow this topic</div>
+                    <div><a href="/" className="text-teal-600 hover:underline">Host events</a></div>
+                  </div>
+                </div>
+              </div>
+              <div className="flex flex-col sm:flex-row -mx-2">
+                <div className="w-full sm:w-1/2 mx-2 bg-white shadow mb-4">
+                  <div className="px-6 py-4">
+                    <div className="font-bold"><a href="/">Technology</a></div>
+                    <div className="text-gray-700 mb-1">10,093 Follow this topic</div>
+                    <div><a href="/" className="text-teal-600 hover:underline">Host events</a></div>
+                  </div>
+                </div>
+
+                <div className="w-full sm:w-1/2 mx-2 bg-white shadow mb-4">
+                  <div className="px-6 py-4">
+                    <div className="font-bold"><a href="/">PHP</a></div>
+                    <div className="mb-1 text-gray-700">1,227 follow this topic</div>
+                    <div><a href="/" className="text-teal-600 hover:underline">Host events</a></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+          </div>
+          <div className="w-full lg:w-3/10 ml-6">
+            <div className="pt-2 pb-4">
+              &nbsp;
+            </div>
+
+            <div className="border border-gray-400 bg-white text-sm mb-8">
+              <div className="border-b border-gray-400 flex px-4 py-2">
+                <a href="/">All upcoming events</a>
+              </div>
+              <div className="border-b border-gray-400 flex font-bold bg-purple-100 text-meetup-purple">
+                <div className="border-l-4 border-meetup-purple px-4 pl-3 py-2">
+                  <a href="/">Your groups and suggestions</a>
+                </div>
+              </div>
+              <div className="border-b border-gray-400 flex px-4 py-2">
+                <a href="/">Your groups only</a>
+              </div>
+              <div className="flex px-4 py-2">
+                <a href="/">Your events only</a>
+              </div>
+            </div>
+
+            <div className="flex lg:justify-end mb-4">
+              <button className="bg-white px-4 py-1 text-sm border border-gray-500">Today</button>
+            </div>
+
+            <div>
+              Calendar here
+            </div>
+
+            <div className="text-center flex justify-center mb-8">
+              <a href="/" className="flex">
+                <svg className="mr-2" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path className="heroicon-ui" d="M19 6.41L8.7 16.71a1 1 0 1 1-1.4-1.42L17.58 5H14a1 1 0 0 1 0-2h6a1 1 0 0 1 1 1v6a1 1 0 0 1-2 0V6.41zM17 14a1 1 0 0 1 2 0v5a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V7c0-1.1.9-2 2-2h5a1 1 0 0 1 0 2H5v12h12v-5z"></path></svg>
+                <span>Export to...</span>
+              </a>
+            </div>
+
+          </div>
+        </div>
+
       </div>
     );
   }

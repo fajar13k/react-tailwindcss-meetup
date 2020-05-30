b78ea382c6e6ef23daf9d43968307b77690c3f29
fajar13k <fajar.fj13@gmail.com>
2020-05-31 05:37:42 +0700

    Add search bar

--- src/App.js
+++ src/App.js
@@ -37,6 +37,25 @@ class App extends Component {
           </div>
         </div>
 
+        {/* Container for middle event finder */}
+        <div className="container mx-auto px-40 mb-16">
+          <div className="bg-gray-900 text-white flex items-center justify-between p-3 -my-8">
+            <div className="flex items-center">
+              <span className="relative">
+                <input type="text" placeholder="Search" className="text-gray-600 rounded px-2 py-3 w-72" />
+                <span className="absolute right-0 top-0 mr-3" style={{ top: "10px" }}>
+                  <svg className="text-gray-600" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path className="heroicon-ui" d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z"></path></svg>
+                </span>
+              </span>
+              <span className="ml-4">within <a href="/" className="border-b border-gray-500 hover:border-gray-100">50 miles</a> of <a href="/" className="border-b border-gray-500 hover:border-gray-100">Mississauga, Ontario</a></span>
+            </div>
+            <div className="flex items-center">
+              <button className="border-l border-b border-t border-gray-700 px-6 py-2">Groups</button>
+              <button className="border-r border-b border-t border-gray-700 bg-gray-700 text-white px-6 py-2 font-bold">Calendar</button>
+            </div>
+          </div>
+        </div>
+
       </div>
     );
   }
